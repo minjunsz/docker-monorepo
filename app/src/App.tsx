63acad3aa7d2c7114b6fdb649dded663e3d09ce2
minjunsz <minjunsz@postech.ac.kr>
2020-08-20 20:16:30 +0900

    verify connection between dockerized apps

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [serverStatus, setServerStatus] = useState("loading...");
+
+  useEffect(() => {
+    fetch('http://localhost:4000/')
+      .then((response: Response) => response.text())
+      .then((text: string) => {
+        setServerStatus(text);
+      })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Entrypoint of React app
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <dl>
+          <dt>Response from Server</dt>
+          <dd>{serverStatus}</dd>
+        </dl>
       </header>
     </div>
   );
